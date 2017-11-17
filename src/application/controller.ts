756358b6db39055df2f51b9ec687d371a4b9815d
chevylevel <chevylevel@gmail.com>
2017-11-17 20:02:57 +0300

    controller was implemented

--- src/application/controller.ts
+++ src/application/controller.ts
@@ -1,4 +1,9 @@
+// import { Request, Response, Router } from "express";
+// import * as fs from 'fs';
+// import * as path from 'path';
 
-export  async function getValueById() {
+// export function getValueById(req: Request, res: Response) {
+//     return new Promise(resolve, reject) {
 
-}
+//     }
+// }
--- src/application/router.ts
+++ src/application/router.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import * as fs from "fs";
 import * as path from "path";
+// import * as controller from "./controller";
 
 export const router: Router = Router();
 
@@ -26,13 +27,55 @@ router.get("/", (req: Request, res: Response) => res.send("Hello world"));
  * @apiSuccess {String} firstname Firstname of the User.
  * @apiSuccess {String} lastname  Lastname of the User.
  */
-router.post("/", async (req: Request, res: Response) => {
-    console.log(req.body);
-    await new Promise((resolve, rej) => {
-        fs.appendFile(path.join(__dirname, "../../storage.json"), req.body.id, (err) => {
-            err ? rej(err) : console.log("success"), resolve();
+router.post("/", async (req: Request, resp: Response) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(
+            path.join(__dirname, "../../storage.json"),
+            "utf8",
+            (err: Error, contents: string) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    const jsObjects: Object[] = JSON.parse(contents);
+                    console.log(typeof jsObjects);
+                    jsObjects.push(req.body);
+                    resolve(jsObjects);
+                }
+            },
+        );
+    })
+    .then((dataToWrite) => {
+        return new Promise((resolve, reject) => {
+            fs.truncate(
+                path.join(__dirname, "../../storage.json"),
+                0,
+                (err: Error) => {
+                    err ? reject(err) : resolve(dataToWrite), console.log("truncated");
+                },
+            );
+        });
+    })
+    .then((dataToWrite) => {
+        return new Promise((resolve, reject) => {
+            fs.appendFile(
+                path.join(__dirname, "../../storage.json"),
+                JSON.stringify(dataToWrite),
+                (err: Error) => {
+                    err ? reject(err) : resolve(), resp.json(dataToWrite);
+                },
+            );
         });
     });
-    res.json(req.body);
-    console.log("responce");
 });
+
+/**
+ * @api {get} /:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+// router.get("/:id", controller.getValueById);
