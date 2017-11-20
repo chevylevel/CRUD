4b878165b255e053587ed4bc365b52a29a15706a
chevylevel <chevylevel@gmail.com>
2017-11-20 17:59:36 +0300

    asyncawait implementation

--- src/application/controller.ts
+++ src/application/controller.ts
@@ -1,7 +1,21 @@
 import { Request, Response } from "express";
 import * as util from "./util";
+import { append, clear } from "./util";
 
 const FILE_PATH: string = "../../storage.json";
+
+export async function createElement(req: Request, res: Response): Promise<Object> {
+    let jsonString = await util.read(FILE_PATH);
+    if (!jsonString) {
+        jsonString = "{}";
+    }
+    const elements = JSON.parse(jsonString);
+    elements[req.body.id] = req.body;
+    await clear(FILE_PATH);
+    await append(elements);
+    res.sendStatus(200);
+    return req.body;
+}
 
 export async function getElementById(req: Request, res: Response): Promise<Object> {
     const jsonString: string = await util.read(FILE_PATH);
@@ -15,8 +29,9 @@ export async function deleteById(req: Request, res: Response): Promise<void> {
     const jsonString: string = await util.read(FILE_PATH);
     const { id } = req.params;
     const elements: Object = JSON.parse(jsonString);
+    const elementToDel: Object = elements[id];
     delete elements[id];
     await util.clear(FILE_PATH);
     await util.append(elements);
-    res.send(200);
+    res.send(elementToDel);
 }
--- src/application/router.ts
+++ src/application/router.ts
@@ -1,6 +1,4 @@
 import { Request, Response, Router } from "express";
-import * as fs from "fs";
-import * as path from "path";
 import * as controller from "./controller";
 
 export const router: Router = Router();
@@ -27,46 +25,7 @@ router.get("/", (req: Request, res: Response) => res.send("Hello world"));
  * @apiSuccess {String} firstname Firstname of the User.
  * @apiSuccess {String} lastname  Lastname of the User.
  */
-router.post("/", async (req: Request, resp: Response) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(
-            path.join(__dirname, "../../storage.json"),
-            "utf8",
-            (err: Error, contents: string) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    const elements: Object = JSON.parse(contents);
-                    const { id } = req.body;
-                    elements[id] = req.body;
-                    resolve(elements);
-                }
-            },
-        );
-    })
-    .then((dataToWrite) => {
-        return new Promise((resolve, reject) => {
-            fs.truncate(
-                path.join(__dirname, "../../storage.json"),
-                0,
-                (err: Error) => {
-                    err ? reject(err) : resolve(dataToWrite);
-                },
-            );
-        });
-    })
-    .then((dataToWrite) => {
-        return new Promise((resolve, reject) => {
-            fs.appendFile(
-                path.join(__dirname, "../../storage.json"),
-                JSON.stringify(dataToWrite),
-                (err: Error) => {
-                    err ? reject(err) : resolve(), resp.json(dataToWrite);
-                },
-            );
-        });
-    });
-});
+router.post("/", controller.createElement);
 
 /**
  * @api {get} /:id Request User information
@@ -90,5 +49,4 @@ router.get("/:id", controller.getElementById);
  * @apiSuccess {String} firstname Firstname of the User.
  * @apiSuccess {String} lastname  Lastname of the User.
  */
-
 router.delete("/:id", controller.deleteById);
